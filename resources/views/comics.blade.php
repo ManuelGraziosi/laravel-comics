c2a816e1d288e23ccabc5f902becb43a4dbb6475
ManuelGraziosi <manuel.graziosi@gmail.com>
2026-04-26 19:09:41 +0200

    feat: comics grid (wip)

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -1,6 +1,27 @@
 @extends("layouts.master")
 
+
+@php
+    // dd(config('comics'))
+    $comics = config('comics');
+@endphp
+
 @section("contenuto")
     <h3>CURRENT SERIES</h3>
     {{-- GRID --}}
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4">
+
+        @foreach ($comics as $comic)
+            <div class="col">
+                <x-card>
+                    <x-slot:thumb>{{ $comic['thumb'] }}</x-slot:thumb>
+                    <x-slot:title>{{ $comic['title'] }}</x-slot:title>
+                    <x-slot:type>{{ $comic['type'] }}</x-slot:type>
+                    <x-slot:title>{{ $comic['title'] }}</x-slot:title>
+                    <x-slot:series>{{ $comic['series'] }}</x-slot:series>
+                    <x-slot:price>{{ $comic['price'] }}</x-slot:price>
+                </x-card>
+            </div>
+        @endforeach
+    </div>
 @endsection
--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,34 @@
 <div>
     <!-- Well begun is half done. - Aristotle -->
+    <div class="card h-100 border-0 bg-dark text-white">
+
+        <img src="{{ $thumb }}"
+             class="card-img-top"
+             alt="{{ $title }}"
+             style="height: 280px; object-fit: cover;">
+
+        <div class="card-body d-flex flex-column">
+            <span class="badge bg-primary align-self-start mb-2 text-uppercase">
+                {{ $type }}
+            </span>
+
+            <h5 class="card-title text-uppercase fw-bold">
+                {{ $title }}
+            </h5>
+
+            <p class="card-text text-secondary mb-1">
+                {{ $series }}
+            </p>
+
+            <p class="fw-bold mb-3">
+                {{ $price }}
+            </p>
+
+            <a href="#"
+               class="btn btn-primary mt-auto text-uppercase fw-bold">
+                Dettagli
+            </a>
+        </div>
+
+    </div>
 </div>
